95e2c47e81c65194b0a555f5b965c56254980cf8
priyanka <priyanka.divekar1100@gmail.com>
2022-08-05 14:55:45 +0530

    Program

---
+++ unitb.sh
@@ -0,0 +1,9 @@
+echo "Read the lenth of rect0angular part feet : "
+read l
+echo "Read the bredth of rectangular part feet: "
+read b
+a=$(($l * $b))
+echo "Area of plot in feet $a"
+meterCon=0.092903
+meter=$(awk 'BEGIN {print '$a' * '$meterCon'}')
+echo " Area in meters $meter"
